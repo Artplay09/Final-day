fbd7ef9009708b29382465dcca9c15335a4ed41a
Artplay09 <74101697+Artplay09@users.noreply.github.com>
2021-05-22 17:47:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const leftArrow = document.getElementsByClassName('arrow left')[0];
+const rightArrow = document.getElementsByClassName('arrow right')[0];
+
+const slides = document.getElementsByClassName('slide'); 
+const dots = document.getElementsByClassName('dots')[0];
+const dot = document.getElementsByClassName('dot');
+let index = 0;
+
+function showSlide(i){
+    console.log(i)
+   for(let j = 0; j < slides.length;j++){
+     slides[j].classList.remove('active')
+   }
+   slides[i].classList.add('active');
+   index = i;
+   dot[index].checked = true;
+}
+leftArrow.addEventListener('click', () => {
+    let currentIndex = index === 0 ? slides.length - 1 : index -1;
+    showSlide(currentIndex);
+})
+rightArrow.addEventListener('click', () => {
+    let currentIndex = index === slides.length -1 ? 0 : index +1;
+    showSlide(currentIndex);
+})
+
+dots.addEventListener('click', (e) => {
+    for(let i = 0; i < dot.length; i++) {
+        if(dot[i] === e.target){
+            showSlide(i);
+            break;
+        }
+    }
+})
